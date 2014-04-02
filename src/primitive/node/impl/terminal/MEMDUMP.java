2d924c7f6c5b6adab9424899ed7296fb8072db9e
mocchi <boom.boom.planet@gmail.com>
2014-04-02 18:57:12 +0900

    MEMDUMP updated

--- src/primitive/node/impl/terminal/MEMDUMP.java
+++ src/primitive/node/impl/terminal/MEMDUMP.java
@@ -12,9 +12,11 @@ public class MEMDUMP implements Node{
     }
     @Override
     public char get() {
-        int no = 0;
+        int no = 0,thisNo = -1;
         char ch = 0;
         Pointer p = env.getPointer();
+        final Pointer finalPointer = p;
+        StringBuffer sb = new StringBuffer();
         //get top
         while(p.hasLeft()){
             p = p.left();
@@ -23,10 +25,14 @@ public class MEMDUMP implements Node{
         System.out.println("\n---- memory dump ----");
         while(p.hasRight()){
             ch = p.getMemory().get();
-            System.out.println("addr:0x"+ Integer.toHexString((no)) + " val:0x" + Integer.toHexString((int)ch));
+            if(p == finalPointer){
+                System.out.println("current pointer - no:0x" + Integer.toHexString((no)));
+            }
+            sb.append("addr:0x"+ Integer.toHexString((no)) + " val:0x" + Integer.toHexString((int)ch) + "\n");
             p = p.right();
             no++;
         }
+        System.out.println(sb);
         System.out.println("---------------------");
         return 0;
     }
